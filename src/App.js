780ebb05ac5917ecee1d3c6158db1ab2a9cb88c9
seungwoohan12 <hsu235@naver.com>
2023-11-10 03:14:51 +0900

    두번째실습

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
+// import './App.css';
 
 import MyHeader from './MyHeader';
 
 function App() {
   let name = "한승우";
 
+  const style = {
+    App: {
+      backgroundColor:"black",
+    },
+    h2: {
+      color:"red",
+    },
+    bold_text: {
+      color:"green",
+    },
+  };
+
   return (
-    <div className="App">
+    <div style={style.App}>
       <MyHeader />
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+        <h2 style={style.h2}>안녕 리액트 {name}</h2>
+        <b style={style.bold_text} id="bold_text">React.js</b>
     </div>
   );
 }
